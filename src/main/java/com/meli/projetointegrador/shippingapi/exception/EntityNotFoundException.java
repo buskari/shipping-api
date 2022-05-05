eba01a3906ca42b80449672f3dc2b90d0f6c18d3
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-05-05 12:44:34 -0300

    add EntityNotFoundException

---
+++ src/main/java/com/meli/projetointegrador/shippingapi/exception/EntityNotFoundException.java
@@ -0,0 +1,24 @@
+package com.meli.projetointegrador.shippingapi.exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class EntityNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EntityNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
