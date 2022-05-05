a3e5fc3690ea6966d9a966d36e6851a13658e4eb
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-05-05 12:35:04 -0300

    add annotation

--- src/main/java/com/meli/projetointegrador/shippingapi/service/ShippingService.java
+++ src/main/java/com/meli/projetointegrador/shippingapi/service/ShippingService.java
@@ -1,9 +1,11 @@
 package com.meli.projetointegrador.shippingapi.service;
 
 import com.meli.projetointegrador.shippingapi.model.Shipping;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface ShippingService {
     List<Shipping> findAll();
     Shipping findById();
