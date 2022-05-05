d2840f41e743536b2aaa2dfd0b9c47a3f68cffb6
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-05-05 16:52:59 -0300

    add @GenerateValue and @JsonFormat

--- src/main/java/com/meli/projetointegrador/shippingapi/model/Shipping.java
+++ src/main/java/com/meli/projetointegrador/shippingapi/model/Shipping.java
@@ -1,8 +1,11 @@
 package com.meli.projetointegrador.shippingapi.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
@@ -14,9 +17,12 @@ import java.time.LocalDateTime;
 @Builder
 public class Shipping {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long trackingCode;
     private String status;
+    @JsonFormat(pattern = "yyyy-MM-ddTHH:mm:ss")
     private LocalDateTime created;
+    @JsonFormat(pattern = "yyyy-MM-ddTHH:mm:ss")
     private LocalDateTime lastUpdated;
     private Long orderId;
 }
