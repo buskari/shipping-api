8cb361327b75c64295dc345ad4a98e56dc80c106
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-05-05 12:34:33 -0300

    refactor class name add annotation

--- src/main/java/com/meli/projetointegrador/shippingapi/repository/ShippingRepository.java
+++ src/main/java/com/meli/projetointegrador/shippingapi/repository/ShippingRepository.java
@@ -2,6 +2,8 @@ package com.meli.projetointegrador.shippingapi.repository;
 
 import com.meli.projetointegrador.shippingapi.model.Shipping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface Repository extends JpaRepository<Shipping, Long> {
+@Repository
+public interface ShippingRepository extends JpaRepository<Shipping, Long> {
 }
