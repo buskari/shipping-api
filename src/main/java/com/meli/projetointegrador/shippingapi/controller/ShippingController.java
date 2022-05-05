605096f45ee00d2b64387c9064954177adde3549
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-05-05 13:02:55 -0300

    remove uri

--- src/main/java/com/meli/projetointegrador/shippingapi/controller/ShippingController.java
+++ src/main/java/com/meli/projetointegrador/shippingapi/controller/ShippingController.java
@@ -3,6 +3,7 @@ package com.meli.projetointegrador.shippingapi.controller;
 import com.meli.projetointegrador.shippingapi.exception.EntityNotFoundException;
 import com.meli.projetointegrador.shippingapi.model.Shipping;
 import com.meli.projetointegrador.shippingapi.service.ShippingService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -11,7 +12,6 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/shipping")
 public class ShippingController {
 
     private final ShippingService shippingService;
@@ -31,11 +31,7 @@ public class ShippingController {
     }
 
     @PostMapping
-    public ResponseEntity<Shipping> create(Shipping shipping, UriComponentsBuilder uriComponentsBuilder) {
-        URI uri = uriComponentsBuilder
-                .path("/api/v1/shipping/{trackingCode}")
-                .buildAndExpand(shipping.getTrackingCode())
-                .toUri();
-        return ResponseEntity.created(uri).body(shippingService.save(shipping));
+    public ResponseEntity<Shipping> create(Shipping shipping) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(shippingService.save(shipping));
     }
 }
